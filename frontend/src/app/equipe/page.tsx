c424ddf23f84d9a7223ec007c45bc41afca6e551
Anderson Beauvais <50684207+Andy7911@users.noreply.github.com>
2024-11-23 23:14:46 -0500

    fist commit

---
+++ frontend/src/app/equipe/page.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export default function Equipe() {
+  return (
+    <div>Equipe</div>
+  )
+}
---
+++ frontend/src/pages/equipe.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function equipe() {
+  return (
+    <div>equipe</div>
+  )
+}
