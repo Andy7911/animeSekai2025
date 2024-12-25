d808d4f9e1bb5bb23052455b1d107defbc89d017
Anderson Beauvais <50684207+Andy7911@users.noreply.github.com>
2024-12-25 14:20:22 -0500

    def

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,70 +4,108 @@ import example from '../../public/img/tokyo-thumb.jpeg'
 import Recommend from "@/components/Recommend";
 import LastRealese from "@/components/LastRealese";
 
-interface Picture{
-  url:String
+interface Picture {
+  url: String
 }
-interface Media{
-  url:String
+interface Media {
+  url: String
 }
-interface Anime{
- id:Number,
- Name:String,
- description:String,
- notation:number,
- Highline_title:String
- categories:String
- gif:Media,
- musique:Media
- 
-}
-
-interface Hero { 
-id:number,
-Headline:String,
-SubHeadline:String,
-Description:String,
-Picture: Picture,
-anime:Anime
+interface Anime {
+  id: Number,
+  Name: String,
+  description: String,
+  notation: number,
+  Highline_title: String
+  categories: String
+  gif: Media,
+  musique: Media
 
 }
-interface Recommends{
-anime:Anime
+
+interface Hero {
+  id: number,
+  Headline: String,
+  SubHeadline: String,
+  Description: String,
+  Picture: Picture,
+  anime: Anime
 
 }
-interface HomeRespose{
-  data:{
-  Hero:Hero,
-  Recommends:Recommends[]
+interface Recommends {
+  anime: Anime
+
 }
+interface HomeRespose {
+  data: {
+    Hero: Hero,
+    Recommends: Recommends[]
+  }
 }
 
 export default async function Home() {
-async function getData():Promise<HomeRespose>{
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/home-page?populate[Hero][populate][anime][populate]=*&populate[Hero][populate]=Picture&populate[Recommends][populate]=*`,{
-    headers:{'Authorization':`Bearer ${process.env.TOKEN_API}  `}
-  })
-  await new Promise((resolve=>setTimeout(resolve,4000)))
-  return res.json();
-} 
+  async function getData(): Promise<HomeRespose> {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/home-page?populate[Hero][populate][anime][populate]=*&populate[Hero][populate]=Picture&populate[Recommends][populate]=*`, {
+      headers: { 'Authorization': `Bearer ${process.env.TOKEN_API}  ` }
+    })
+    await new Promise((resolve => setTimeout(resolve, 4000)))
+    return res.json();
+  }
 
-let res = await getData();
-const {Hero , Recommends} = res.data;
-console.log(Hero.anime);
+  let res = await getData();
+  const { Hero, Recommends } = res.data;
+  console.log(Hero.anime);
   return (
     <div className="container home">
-      <HeroComponent  Headline={Hero.Headline} Picture={Hero.Picture} Description={Hero.Description} Anime={Hero.anime} gif={Hero.anime.gif}  SubHeadline={Hero.SubHeadline} mu/>
+      <div className="home__left">
+        <HeroComponent Headline={Hero.Headline} Picture={Hero.Picture} Description={Hero.Description} Anime={Hero.anime} gif={Hero.anime.gif} SubHeadline={Hero.SubHeadline} mu />
 
-   
-    
- <Recommend />
-      
-    <LastRealese/>
-    
-<section className="watching" style={{height:500,width:1200}}>
-  <h4>Vos visionnnement </h4>
 
-</section>
+        <Recommend />
+
+        <LastRealese />
+
+        <section className="watching" style={{ height: 500, width: 1200 }}>
+          <h4>Vos visionnnement </h4>
+
+        </section>
+      </div>
+      <div className="home__right">
+
+        <div className="filter">
+          <h4>Quick filter</h4>
+          <div className="filter__content">
+            <div className="filter__item">
+              <div className="filter__dropdown">
+
+                Genre <span> All</span>
+              </div>
+              <div className="filter__menu">
+
+              </div>
+
+            </div>
+            <div className="filter__item">
+              <div className="filter__dropdown">
+
+                Genre <span> All</span>
+              </div>
+              <div className="filter__menu">
+
+              </div>
+
+            </div>
+
+
+          </div>
+          <input type="text" />
+
+          <a className="btn"> 
+<span className="btn__text">Filter</span>
+          </a>
+
+        </div>
+
+      </div>
     </div>
   );
 }
